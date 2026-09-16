fedc9dbd6b535109b96c0206a3e8de2218608b30
lokesh-2411 <lokeshssbk007@gmail.com>
2026-09-16 21:04:51 +0530

    ✅ Find All Anagrams in a String | Medium | C++ | Runtime: 4 ms (68.48%) | Memory: 11.5 MB | 2026-09-16

---
+++ leetcode/medium/0438-find-all-anagrams-in-a-string/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> ans;
+        int n = s.size();
+        int m = p.size();
+        if(n < m) return ans;
+
+        vector<int> freqP(26, 0), freqS(26, 0);
+
+        for(char c : p) freqP[c - 'a']++;
+
+        for(int i = 0 ; i < n ; i++){
+            freqS[s[i] - 'a']++;
+
+            if(i >= m){
+                freqS[s[i - m] - 'a']--;
+            }
+
+            if(freqS == freqP){
+                ans.push_back(i - m + 1);
+            }
+        }
+        return ans;
+    }
+};
