71fd75c14226bd24ebf4a95bf12192e505116d2f
lokesh-2411 <lokeshssbk007@gmail.com>
2026-09-24 09:51:41 +0530

    ✅ Smallest Index With Digit Sum Equal to Index | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 30.9 MB | 2026-09-24

---
+++ leetcode/easy/3550-smallest-index-with-digit-sum-equal-to-index/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int smallestIndex(vector<int>& nums) {
+        int n = nums.size();
+
+        for(int i = 0 ; i < n ; i++){
+            int num = nums[i];
+            int digitSum = 0;
+
+            while(num > 0){
+                digitSum += num % 10;
+                num /= 10;
+            }
+
+            if(digitSum == i) return i;
+        }
+        return -1;
+    }
+};
